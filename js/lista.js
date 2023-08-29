17a56034207c207980d78bf595a8eb4f307783e1
MirellaRossi <rm97990@fiap.com.br>
2023-08-28 23:14:12 -0300

    lista de tarefas

---
+++ js/lista.js
@@ -0,0 +1,51 @@
+// Função para adicionar uma nova tarefa à lista
+function adicionarTarefa() {
+    const descricao = document.getElementById("Desc").value;
+    const autor = document.getElementById("author").value;
+    const departamento = document.getElementById("depart").value;
+    const importancia = document.getElementById("impor").value;
+    const valor = document.getElementById("valor").value;
+    const duracao = document.getElementById("duracao").value;
+
+    const tarefa = {
+        descricao,
+        autor,
+        departamento,
+        importancia,
+        valor,
+        duracao
+    };
+
+    const taskList = document.getElementById("taskList");
+    const listItem = document.createElement("li");
+    //Define a nova lista 
+    listItem.innerHTML = `${descricao} - ${autor} - ${departamento} - ${importancia}`;
+    taskList.appendChild(listItem);
+
+    // Adicionando as informações preenchidas no formulário
+    document.getElementById("Desc").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("depart").value = "";
+    document.getElementById("impor").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("duracao").value = "";
+}
+
+// Adiciona o evento de clique no botão "Adicionar tarefa"
+document.getElementById("btLista").addEventListener("click", adicionarTarefa);
+
+// Função para excluir tarefas concluídas
+function excluirTarefasConcluidas() {
+    const taskList = document.getElementById("taskList");
+    const tarefas = taskList.getElementsByTagName("li");
+
+    for (let i = tarefas.length - 1; i >= 0; i--) {
+        const tarefa = tarefas[i];
+        if (tarefa.classList.contains("concluida")) {
+            taskList.removeChild(tarefa);
+        }
+    }
+}
+
+// Adiciona o evento de clique no botão "Excluir tarefas concluídas"
+document.getElementById("btExcluirConcluidas").addEventListener("click", excluirTarefasConcluidas);
