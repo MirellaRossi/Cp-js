720ed6b4d4cadf9a95eeecc37348be18efca7fac
MirellaRossi <rm97990@fiap.com.br>
2023-08-28 22:18:07 -0300

    alteração dos textos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const calcularbot = document.getElementById("calculo");
 const resultadobot = document.getElementById("resultado");
-
+//Evento para calcular o aumenbto do salário quando clicar no botão
 calcularbot.addEventListener("click", () => {
     const salario = [1500, 1700, 2200, 1900, 2500, 2700, 2100, 3000, 1680, 2300];
 //Criação da váriavel a qual o aumento será atribuido
@@ -20,7 +20,7 @@ calcularbot.addEventListener("click", () => {
 
 //A partir do  do resultado do cálculo, usamos o inner.html para mostrar ao usuário os tópicos acima
     resultadobot.innerHTML = `
-        <p>Salários Atualizados: ${(salarioN)}</p> 
+        <p>Salários Atualizados em 10% quando maiores que R$2000 e em 15% quando menores ou iguais a R$2000: ${(salarioN)}</p> 
         <p>Salários Superiores a 2500: ${(SDQ)}</p>
         <p>Soma dos Salários Superiores a 2500: ${soma}</p>
     `;
